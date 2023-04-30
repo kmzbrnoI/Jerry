94f2be19e8033831bfb06453c4f19ec0939cfa50
Jan Horacek <me@apophis.cz>
2023-04-30 19:32:20 +0200

    Upgrade ListeningThread.

--- src/ListeningThread.pas
+++ src/ListeningThread.pas
@@ -1,30 +1,33 @@
 unit ListeningThread;
 
 {
-  Vlakno poslouchajici pakety na TCP spojeni
+  Poslouchaci vlakno TCP spojeni
 }
 
 interface
 
 uses
-  Classes, IdTCPClient;
+  Classes, IdTCPClient, SysUtils, IdException;
 
 type
   TDataEvent = procedure(const Data: string) of object;
-  TTimeoutEvent = procedure() of object;
+  TErrorEvent = procedure() of object;
+
   TReadingThread = class(TThread)
   private
     FClient: TIdTCPClient;
     FData: string;
     FOnData: TDataEvent;
-    FOnTimeout: TTimeoutEvent;
+    FOnError: TErrorEvent;
+    FOnTimeout: TErrorEvent;
     procedure DataReceived;
   protected
     procedure Execute; override;
   public
     constructor Create(AClient: TIdTCPClient); reintroduce;
     property OnData: TDataEvent read FOnData write FOnData;
-    property OnTimeout: TTimeoutEvent read FOnTimeout write FOnTimeout;
+    property OnError: TErrorEvent read FOnError write FOnError;
+    property OnTimeout: TErrorEvent read FOnTimeout write FOnTimeout;
   end;
 
 implementation
@@ -40,11 +43,20 @@ begin
   while (not Terminated) do
   begin
     try
-     FData := FClient.IOHandler.ReadLn;
+      FData := FClient.IOHandler.ReadLn();
     except
-     if ((Assigned(FOnTimeout)) and (not Terminated)) then
-      Synchronize(FOnTimeout);
-     Exit;
+      on E: EIdConnClosedGracefully do
+      begin
+        if ((Assigned(FOnTimeout)) and (not Terminated)) then
+          Synchronize(FOnTimeout);
+        Exit();
+      end;
+      on E: Exception do
+      begin
+        if ((Assigned(FOnError)) and (not Terminated)) then
+          Synchronize(FOnError);
+        Exit();
+      end;
     end;
     if (FData <> '') and Assigned(FOnData) then
       Synchronize(DataReceived);
